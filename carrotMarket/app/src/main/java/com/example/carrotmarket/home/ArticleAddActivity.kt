b9c2437f567afe687793ec2086fa810349ded37b
jaino <msb3302@naver.com>
2022-03-29 22:50:43 +0900

    firebase storage connect

--- carrotMarket/app/src/main/java/com/example/carrotmarket/home/ArticleAddActivity.kt
+++ carrotMarket/app/src/main/java/com/example/carrotmarket/home/ArticleAddActivity.kt
@@ -2,15 +2,16 @@ package com.example.carrotmarket.home
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.carrotmarket.ArticleModel
-import com.example.carrotmarket.R
 import com.example.carrotmarket.databinding.ActivityArticleAddBinding
 import com.example.carrotmarket.utils.Companion.DB_ARTICLES
 import com.google.firebase.auth.FirebaseAuth
@@ -24,14 +25,22 @@ import com.google.firebase.storage.ktx.storage
 class ArticleAddActivity : AppCompatActivity() {
 
     private val binding by lazy{ ActivityArticleAddBinding.inflate(layoutInflater)}
+    private var selectedUri: Uri? = null
     private val getContent =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result: ActivityResult ->
-            binding.photoImageView.setImageURI(result.data?.data)
+            val uri = result.data?.data
+            if(uri != null) {
+                binding.photoImageView.setImageURI(result.data?.data)
+                binding.photoImageView.setImageURI(uri)
+                selectedUri = uri
+            }else{
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+
     private val auth: FirebaseAuth by lazy{ Firebase.auth }
     private val storage: FirebaseStorage by lazy{ Firebase.storage }
     private val articleDB: DatabaseReference by lazy{ Firebase.database.reference.child(DB_ARTICLES)}
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +59,10 @@ class ArticleAddActivity : AppCompatActivity() {
                     showPermissionContextPopup()
                 }
                 else->{
-
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1010
+                    )
                 }
             }
         }
@@ -59,11 +71,44 @@ class ArticleAddActivity : AppCompatActivity() {
             val price = binding.priceEditText.text.toString()
             val sellerId = auth.currentUser?.uid.orEmpty()
 
-            val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", "")
-            articleDB.push().setValue(model)
+            if(selectedUri != null){
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadPhoto(photoUri,
+                    successHandler = { uri ->
+                        uploadArticle(sellerId, title, price, uri)
+                    },
+                    errorHandler = {
+                        Toast.makeText(this, "사진 업로드에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }else{
+                uploadArticle(sellerId, title, price, "")
+            }
 
-            finish()
         }
+    }
+    private fun uploadPhoto(uri: Uri, successHandler: (String)-> Unit, errorHandler: ()->Unit){
+        val fileName = "${System.currentTimeMillis()}.png"
+        storage.reference.child("article/photo").child(fileName)
+            .putFile(uri)  // 저장
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    storage.reference.child("article/photo").child(fileName)
+                        .downloadUrl // 저장 성공하면 downloadUri를 가져옴
+                        .addOnCompleteListener{ uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener{
+                            errorHandler()
+                        }
+                }else{
+                    errorHandler()
+                }
+            }
+    }
+    private fun uploadArticle(sellerId: String, title: String, price:String, imageUri: String){
+        val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", imageUri)
+        articleDB.push().setValue(model)
+        finish()
     }
 
     override fun onRequestPermissionsResult(
