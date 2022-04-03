39c6fc82ab65e75554023e4e128286b4a9c0ba59
jaino <msb3302@naver.com>
2022-04-03 17:03:20 +0900

    using tmap api, create search system

--- NotificationFinder/app/src/main/java/com/example/notificationfinder/MainActivity.kt
+++ NotificationFinder/app/src/main/java/com/example/notificationfinder/MainActivity.kt
@@ -2,28 +2,45 @@ package com.example.notificationfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notificationfinder.databinding.ActivityMainBinding
 import com.example.notificationfinder.model.LocationLatLngEntitiy
 import com.example.notificationfinder.model.SearchResultEntity
+import com.example.notificationfinder.utils.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
     private lateinit var adapter :SearchRecyclerAdapter
 
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        job = Job()
+
         initAdapter()
         binding.searchRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.searchRecyclerView.adapter = adapter
         setData()
         adapter.notifyDataSetChanged()
+        bindViews()
         setContentView(binding.root)
     }
     private fun initAdapter(){
         adapter = SearchRecyclerAdapter()
+    }
+    private fun bindViews() = with(binding){
+        searchButton.setOnClickListener {
+            searchKeyWord(searchTextView.text.toString())
+        }
     }
     private fun setData(){
         val dataList = (0..10).map{
@@ -39,6 +56,28 @@ class MainActivity : AppCompatActivity() {
         }
         adapter.setSearchResultList(dataList){
             Toast.makeText(this, "빌딩이름: ${it.buildingName} 주소: ${it.fullAddress}", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun searchKeyWord(keyWordString: String){
+        launch(coroutineContext) { //mainThread
+            try{
+                withContext(Dispatchers.IO){
+                    val response = RetrofitUtil.apiService.getSearchLocation(
+                        keyword = keyWordString
+                    )
+                    if(response.isSuccessful){
+                        val body = response.body()
+                        withContext(Dispatchers.Main){
+                            Log.e("response",body.toString())
+                        }
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "에러가 발생하였습니다. ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
         }
     }
 }
